d4748509b5499a6a1051a4fd4729111179be05e4
Viktor Sherstiuk <vishersb@gmail.com>
2021-02-23 00:32:26 +0200

    last fix

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 const express = require("express");
 var cors = require('cors');
-const {sendNotifications} = require('./modules')
 const app = express();
 const { SendGrid } = require('./utils/SendGrid');
-const {getUsers} = require('./mongo/helpers');
 app.use(cors())
 app.use(express.static(__dirname + "/public"));
 SendGrid.initialize()
@@ -12,11 +10,8 @@ require('./routes')(app);
 mongoose.connect("mongodb+srv://visher:chatbot@cluster0.mxirp.mongodb.net/NewsWebsite?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }, function (err) {
     if (err) return console.log(err);
     app.listen(3002, async function () {
-        //await getUsers()
-        //await sendNotifications()
-        console.log("Сервер ожидает подключения...");
+        console.log("Notify server is on...");
     });
-    // SendGrid.sendMessage({})
 
 });
 
